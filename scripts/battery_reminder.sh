146029436d5603f32bf60b55892f74f18fb6b7ab
Ali Raeisdanaei <mrrookie2@gmail.com>
2020-09-14 18:31:04 -0400

    Changes to config. Swapped to espeak for the battery reminder.

--- scripts/battery_reminder.sh
+++ scripts/battery_reminder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-zenity --error --text '<span foreground="green" font="32" >\n\nBattery Reminder Working</span>' --title="Working!" --width=800 --height=400
+zenity --error --text '<span foreground="green" font="32" >\n\nBattery Reminder Working.</span>' --title="Working!" --width=800 --height=400
 
 
 while [ true ]
@@ -14,8 +14,8 @@ do
 
     #this line extracts if the battery is Discharging, or not. 
     battery_status=`acpi | cut -d " " -f3`
-    LOW=40
-    CRITICAL_LOW=25
+    LOW=25
+    CRITICAL_LOW=15
 
 
 
@@ -26,18 +26,20 @@ do
         then
         if [ $battery_percent -le $CRITICAL_LOW ]
             then 
-            mplayer ~/.config/i3/scripts/sounds/WindowsXp_LowBattery_Sound.mp3    
+            # mplayer ~/.config/i3/scripts/sounds/WindowsXp_LowBattery_Sound.mp3
+            espeak "Your batter is critically low."    
             zenity --error --text '<span foreground="red" font="32" >\n\nBattery level is CRITICAL!\nPLUG IN YOUR COMPUTER NOW!</span>' --title="CRITICAL BATTERY LEVEL!" --width=800 --height=400
 
 
         elif [ $battery_percent -le $LOW ]
             then
-            mplayer ~/.config/i3/scripts/sounds/WindowsXp_LowBattery_Sound.mp3    
+            # mplayer ~/.config/i3/scripts/sounds/WindowsXp_LowBattery_Sound.mp3  
+            espeak "Your battery is low."  
             zenity --error --text '<span foreground="blue" font="32" >\n\nBattery level is LOW!\nPLUG IN YOUR COMPUTER!</span>' --title="LOW BATTERY LEVEL!" --width=800 --height=400
 
         fi
     fi
 
-    sleep 200
+    sleep 260
 
 done
